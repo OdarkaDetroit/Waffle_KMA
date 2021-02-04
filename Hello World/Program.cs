7e244b60503b8c055909f7aa195f4ed4cd6602aa
Daria Shmielova <dasha.shmyelova@outlook.com>
2021-02-04 18:03:58 +0200

    FIrst add

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -6,7 +6,20 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string pangram = "The quick brown fox jumps over the lazy dog";
+
+            string[] message = pangram.Split(' ');
+            string[] newMessage = new string[message.Length];
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                char[] letters = message[i].ToCharArray();
+                Array.Reverse(letters);
+                newMessage[i] = new string(letters);
+            }
+
+            string result = String.Join(" ", newMessage);
+            Console.WriteLine(result);
         }
     }
 }
